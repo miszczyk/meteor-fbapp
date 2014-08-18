edabd277c64248bbad94eb48879162292ccc2148
Mariusz Miszczykowski <miszczyk1@gmail.com>
2014-08-18 19:40:31 +0200

    Update home.js

--- client/home/home.js
+++ client/home/home.js
@@ -1,15 +1,16 @@
 Template.home.rendered = function(){
 	Deps.autorun(function(){
-	Meteor.subscribe("posts",Meteor.userId());
-	Meteor.subscribe("likes");
-	Meteor.subscribe("appusers");
-})
+		Meteor.subscribe("posts",Meteor.userId());
+		Meteor.subscribe("likes");
+		Meteor.subscribe("appusers");
+	})
 
 }
 Template.home.posts = function(){
 	return Posts.find({parent:null},{sort:{date:-1}});
 }
-	Template.home.events({
+
+Template.home.events({
 	'keyup .posttext':function(evt,tmpl){
 		if(evt.which === 13){
 			var posttext = tmpl.find('.posttext').value;
