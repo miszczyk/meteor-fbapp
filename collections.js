032e583b67f0cd5ead362b7187f6bebd94a9a71c
Mariusz Miszczykowski <miszczyk1@gmail.com>
2014-08-18 19:38:07 +0200

    Update collections.js

--- collections.js
+++ collections.js
@@ -1,2 +1,34 @@
 Posts = new Meteor.Collection('posts');
 Likes = new Meteor.Collection('likes');
+
+/* ONLY FOR DEBUG/TESTING PURPOUSES */
+Likes.allow({
+    insert : function(userID)
+    {
+        return true;
+    },
+    update : function(userID)
+    {
+        return true;
+    },
+    remove : function(userID)
+    {
+        return true;
+    }
+});
+
+
+Posts.allow({
+    insert : function(userID)
+    {
+        return true;
+    },
+    update : function(userID)
+    {
+        return true;
+    },
+    remove : function(userID)
+    {
+        return true;
+    }
+});
