be4ae2dce399edbc587c5d49b00ea8503cb9c3b3
Mariusz Miszczykowski <miszczyk1@gmail.com>
2014-08-18 19:41:13 +0200

    Update post.js

--- client/post/post.js
+++ client/post/post.js
@@ -1,23 +1,22 @@
 Template.post.likeCount = function(){
-	return Likes.find(this._id).count();
+	return Likes.find({post:this._id}).count();
 }
-
 
 Template.post.postComments = function(){
 	return Posts.find({parent:this._id});
 }
 Template.post.events({
 	'keyup .comment':function(evt,tmpl){
-	if(evt.which === 13){
-
-		var commenttext = tmpl.find('.comment').value;
-		var options = {text:commenttext,parent:this._id};
-		Meteor.call('addPost',options);
-		Meteor.call('addNames');
-	$('.comment').val('').select().focus();
+		if(evt.which === 13){
+			
+			var commenttext = tmpl.find('.comment').value;
+			var options = {text:commenttext, parent:this._id};
+			Meteor.call('addPost',options);
+			Meteor.call('addNames');
+			$('.comment').val('').select().focus();
 		}
+	},
+	'click #lik' : function(evt, tmpl) {
+		Meteor.call('addLike',{post : this._id});
 	}
-
-
 })
-
